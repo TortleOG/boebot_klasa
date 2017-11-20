8c3819eb9f95ebbefc74922f4651449d916ef6c1
Adam Mahood <mahood.adam@gmail.com>
2017-11-20 17:50:09 -0500

    src: better version check

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ const BoebotClient = require('./structures/Boebot');
 
 const { token } = require('../settings.json');
 
+const vMASTER = 'v8.9';
+const [MAJOR, MINOR] = process.version.split('.');
+
+if (vMASTER !== `${MAJOR}.${MINOR}`) throw `This bot requires a node version of atleast ${vMASTER} You have version ${process.version} installed. Please install the required version.`;
+
 const client = new BoebotClient({
 	prefix: 'b!',
 	cmdEditing: true,
 	provider: { engine: 'rethinkdb' },
 	clientOptions: { fetchAllMembers: true }
 });
-
-if (process.version !== 'v8.9.0') return client.console.error(`This bot requires a node version of atleast v8.9.1 You have version ${process.version} installed. Please install the required version.`);
 
 return client.login(token);
