0e28ed137fcf9dbfdbf3b3168a1fd189f230f187
Adam Mahood <mahood.adam@gmail.com>
2017-11-20 13:05:28 -0500

    src: add version check

--- src/index.js
+++ src/index.js
@@ -9,4 +9,6 @@ const client = new BoebotClient({
 	clientOptions: { fetchAllMembers: true }
 });
 
-client.login(token);
+if (process.version !== 'v8.9.0') return client.console.error(`This bot requires a node version of atleast v8.9.1 You have version ${process.version} installed. Please install the required version.`);
+
+return client.login(token);
