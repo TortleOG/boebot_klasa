e271c0e0b6459b95c7d09dcd59cd3c90ee36cc0b
Adam Mahood <mahood.adam@gmail.com>
2017-12-05 22:09:34 -0500

    src: a few changes
    - Add custom perm levels
    - Add validate function to validate guildSchema

--- src/structures/Boebot.js
+++ src/structures/Boebot.js
@@ -1,9 +1,27 @@
 // Packages
-const { Client } = require('klasa');
+const { Client, PermissionLevels } = require('klasa');
 const { Collection } = require('discord.js');
 
 // Externals
 const Currency = require('./Currency');
+
+// Add custom perm levels
+const permissionLevels = new PermissionLevels()
+	.addLevel(0, false, () => true)
+	.addLevel(2, false, (client, msg) => {
+		if (!msg.guild || !msg.guild.settings.modRole) return false;
+		const modRole = msg.guild.roles.get(msg.guild.settings.modRole);
+		return modRole && msg.member.roles.has(modRole.id);
+	})
+	.addLevel(3, false, (client, msg) => {
+		if (!msg.guild || !msg.guild.settings.adminRole) return false;
+		const adminRole = msg.guild.roles.get(msg.guild.settings.adminRole);
+		return adminRole && msg.member.roles.has(adminRole.id);
+	})
+	.addLevel(6, false, (client, msg) => msg.guild && msg.member.permissions.has('MANAGE_GUILD'))
+	.addLevel(7, false, (client, msg) => msg.guild && msg.member === msg.guild.owner)
+	.addLevel(9, true, (client, msg) => msg.author === client.owner)
+	.addLevel(10, false, (client, msg) => msg.author === client.owner);
 
 module.exports = class BoebotClient extends Client {
 
@@ -14,15 +32,27 @@ module.exports = class BoebotClient extends Client {
 
 		this.currency = null;
 
+		this.permissionLevels = permissionLevels;
+
 		this.once('ready', this.setup.bind(this));
 	}
 
 	async login(token) {
+		await this.validate();
 		return super.login(token);
 	}
 
 	async setup() {
 		this.currency = new Currency(this);
+	}
+
+	async validate() {
+		if (!this.ready) return setTimeout(() => this.validate(), 1000);
+		else if (!this.settings.guilds.schema.modlog) await this.settings.guilds.add('modlog', { type: 'TextChannel' });
+		else if (!this.settings.guilds.schema.muterole) await this.settings.guilds.add('muterole', { type: 'Role' });
+		else if (!this.settings.guilds.schema.modRole) await this.settings.guilds.add('modRole', { type: 'Role' });
+		else if (!this.settings.guilds.schema.adminRole) await this.settings.guilds.add('adminRole', { type: 'Role' });
+		return null;
 	}
 
 };
