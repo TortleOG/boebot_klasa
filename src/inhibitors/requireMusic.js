561693daaa9cc102c09161a688b3a48e3be87b8a
Adam Mahood <mahood.adam@gmail.com>
2018-01-12 20:55:17 -0500

    src: add check for music channel
    * Adds a check to see if there is a music text channel in the guild configs

--- src/inhibitors/requireMusic.js
+++ src/inhibitors/requireMusic.js
@@ -7,13 +7,24 @@ module.exports = class extends Inhibitor {
 	}
 
 	async run(msg, cmd) {
-		if (cmd.requireMusic !== true) return;
+		if (cmd.category === 'Music' && msg.guild.configs.music.musicTC) {
+			if (msg.channel.id !== msg.guild.configs.musicTC) {
+				return [
+					`❌ | ${msg.author}, please head over to `,
+					`${msg.guild.channels.get(msg.guild.configs.music.musicTC)} and use this command.`
+				].join('');
+			}
+		}
+
+		if (cmd.requireMusic !== true) return undefined;
 
 		if (msg.channel.type !== 'text') throw `❌ | ${msg.author}, this command can only be executed inside a server.`;
 
 		if (!msg.member.voiceChannel) throw `❌ | ${msg.author}, you are not connected to a voice channel.`;
 		if (!msg.guild.me.voiceChannel) throw `❌ | ${msg.author}, I am not connected to a voice channel.`;
 		if (msg.member.voiceChannel.id !== msg.guild.me.voiceChannel.id) throw `❌ | ${msg.author}, you must be in the same voice channel as me.`;
+
+		return undefined;
 	}
 
 };
