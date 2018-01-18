8253bd66888de12e8f6444663cb3fecc09fcd794
Adam Mahood <mahood.adam@gmail.com>
2018-01-18 16:36:47 -0500

    fix: modlog.case = null
    * Fixes modlog.case being null on every push

--- src/lib/structures/ModLog.js
+++ src/lib/structures/ModLog.js
@@ -47,7 +47,11 @@ module.exports = class ModLog {
 
 	async getCase() {
 		const row = await this.provider.get('modlogs', this.guild.id);
-		if (!row) return this.provider.create('modlogs', this.guild.id, { modlogs: [this.pack] }).then(() => 1);
+		if (!row) {
+			this.case = 1;
+			return this.provider.create('modlogs', this.guild.id, { modlogs: [this.pack] }).then(() => 1);
+		}
+		this.case = row.modlogs.length + 1;
 		row.modlogs.push(this.pack);
 		await this.provider.replace('modlogs', this.guild.id, row);
 		return row.modlogs.length;
